e2358c1a50ae1a59b721f88b93acd4892cc4879b
Amanda Porto <amandaporto_93@hotmail.com>
2024-01-11 00:12:42 -0300

    Adicionado Desafio Classificador de nível de Herói

---
+++ classificadorNívelHerói/index.js
@@ -0,0 +1,27 @@
+let nomeHeroi = "Tony Stark"
+let nivelXP = 10002
+
+if (nivelXP <= 1000) {
+	console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro")
+    }
+    else if (nivelXP >= 1001 && nivelXP <= 2000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze")
+    } 
+    else if (nivelXP >= 2001 && nivelXP <= 5000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata")
+    }
+    else if (nivelXP >= 5001 && nivelXP <= 7000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro")
+    }
+    else if (nivelXP >= 7001 && nivelXP <= 8000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina")
+    }
+    else if (nivelXP >= 8001 && nivelXP <= 9000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente")
+    }
+   else if (nivelXP >= 9001 && nivelXP <= 10000){ 
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Imortal")
+    }
+   else if (nivelXP >=10001){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Radiante")
+    }
